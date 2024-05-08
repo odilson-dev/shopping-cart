cbd2906b914496d550c2cd30decaa8310639e1ba
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-05-08 11:09:56 -0400

    Finish adding Card details to SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,17 @@
 function SideBar({ isVisible, handleSideBarVisibility, listOfItems }) {
+  const listOfPurchasedItems = listOfItems.map((item) => {
+    return (
+      <div key={item.id}>
+        <h3>{item.title}</h3>
+        <p>Quantity: {item.quantity}</p>
+        <p>Price: ${item.price * item.quantity}</p>
+      </div>
+    );
+  });
+  const totalPrice = listOfItems.reduce((accumulator, item) => {
+    return (accumulator += item.price * item.quantity);
+  }, 0);
+
   return (
     <div id="sidebar" className={isVisible ? "sidebar visible" : "sidebar"}>
       <div className="sidebar-header">
@@ -6,9 +19,15 @@ function SideBar({ isVisible, handleSideBarVisibility, listOfItems }) {
         <box-icon name="x" onClick={handleSideBarVisibility}></box-icon>
       </div>
 
-      <p>Product 1: 2 units</p>
-      <p>Product 2: 3 units</p>
-      <p>Total: $50</p>
+      {listOfPurchasedItems.length == 0 ? (
+        <p>
+          The Card is empty,
+          <br /> start adding items to the Cart
+        </p>
+      ) : (
+        listOfPurchasedItems
+      )}
+      <p className="total">Total: ${totalPrice}</p>
     </div>
   );
 }
