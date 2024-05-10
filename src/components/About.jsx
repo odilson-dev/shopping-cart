6078bc1cb023efdb5dc78d046eddba65b00262de
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-05-10 09:48:12 -0400

    Complete the About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,32 @@ class About extends Component {
   render() {
     return (
       <>
-        <h1>About</h1>
+        <div className="home">
+          <div className="about-cover">
+            <div className="about-box">
+              <h2>About</h2>
+              <p>
+                This app was made with love and passion by{" "}
+                <a target="_blank" href="https://github.com/odilson-dev">
+                  {" "}
+                  Odilson Woodly Italis
+                </a>
+                . Odilson learned to code from{" "}
+                <a target="_blank" href="https://theodinproject.com">
+                  The Odin Project
+                </a>
+                . Check the{" "}
+                <a
+                  target="_blank"
+                  href="https://github.com/odilson-dev/shopping-cart"
+                >
+                  project's repository
+                </a>
+              </p>
+              <div className="about-link-box"></div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
